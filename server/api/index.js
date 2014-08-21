c186fee435587d00543efec025dda6b29470f28a
Desiree Santos <dsantos@thoughtworks.com>
2014-08-21 00:45:50 -0300

    Fixing error

--- server/api/index.js
+++ server/api/index.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var app = module.exports = express();
 var Machine =  require('./service/machine');
- var redis = require("redis"),
-        client = redis.createClient(10553, 'pub-redis-10553.us-east-1-3.1.ec2.garantiadata.com');
 
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.put('/:id', function (req, res) {
-  client.set("status", req.body.status);
   updatedWebClient(req.body.status);
   res.json(new Machine(req.body.status));
 });
 
 app.get('/', function (req, res) {
-  res.json({level: client.get("status")});
+  res.json({level: '70'});
  });
 
 function updatedWebClient(status){
