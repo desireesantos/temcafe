0926b63df852eab90072404814c1a9b0ff54d1cf
Desiree Santos <dsantos@thoughtworks.com>
2014-08-21 00:11:30 -0300

    empty

--- config/default.js
+++ config/default.js
@@ -1,5 +1,9 @@
 module.exports = {
 	server: {
-    port: process.env.PORT || 3000
+    	port: process.env.PORT || 3000
+	},
+	db:{
+		 port: 10553,
+		 hostname: 'pub-redis-10553.us-east-1-3.1.ec2.garantiadata.com'		
 	}
 };
--- server/api/index.js
+++ server/api/index.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var app = module.exports = express();
 var Machine =  require('./service/machine');
-var ws = require('../ws')
+var ws = require('../ws');
+var db = require('../api');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.put('/:id', function (req, res) {
+  db.write(req.body.status);	
   updatedWebClient(req.body.status);
   res.json(new Machine(req.body.status));
 });
 
 app.get('/', function (req, res) {
-  res.json({level: '90'});
+  res.json({level: db.read});
  });
 
 function updatedWebClient(status){
