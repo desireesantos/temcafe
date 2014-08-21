1b7fd0d9f6db7cb9ad9b20820743cceeead64063
Desiree Santos <dsantos@thoughtworks.com>
2014-08-21 00:14:01 -0300

    Adding redis cloud to store in memory

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = module.exports = express();
 var Machine =  require('./service/machine');
 var ws = require('../ws');
-var db = require('../api');
+
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
