3ad511fa2258ddb7e720553d8694421c4b640752
Desiree Santos <dsantos@thoughtworks.com>
2014-10-09 18:47:44 -0300

    Adding redis

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = module.exports = express();
 var Machine =  require('./service/machine');
 
+var redis = require('redis');
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
@@ -11,7 +17,7 @@ app.put('/:id', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-  res.json({level: '70'});
+  res.json({level: });
  });
 
 function updatedWebClient(status){
