9f1a5dd626b891b4e20dfee6168d84709f314662
Desiree Santos <dsantos@thoughtworks.com>
2014-10-10 00:38:40 -0300

    Adding redis to store status data

--- server/api/index.js
+++ server/api/index.js
@@ -2,12 +2,18 @@ var express = require('express');
 var app = module.exports = express();
 var Machine =  require('./service/machine');
 
+var redis = require('redis');
+var url = require('url');
+
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.put('/:id', function (req, res) {
   client.set('coffee', req.body.status);
-  updatedWebClient(parseInt(getRedis()) );
+  updatedWebClient(parseInt(getRedis()));
   res.json(new Machine(req.body.status));
 });
 
@@ -17,4 +23,10 @@ app.get('/', function (req, res) {
 
 function updatedWebClient(status){
 	ws.callSocket().emit('coffe:level', status); 
+}
+
+function getRedis(){
+client.get('coffee', function (err, reply) {
+   reply.toString(); 
+});
 }
