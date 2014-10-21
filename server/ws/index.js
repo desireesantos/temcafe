936c723ca9d5b476c1533cd259e78208f781df93
Desiree Santos <dsantos@thoughtworks.com>
2014-10-21 21:37:13 -0200

    Call websocket

--- server/ws/index.js
+++ server/ws/index.js
@@ -1,10 +1,12 @@
 var socket;
+var redis = require('redis');
 
 exports.listen = function (http) {
  socket = require('socket.io').listen(http, require('config').server);
  socket.sockets.on('connection', function (client) {
       client.emit('coffe:level', '0' );
       console.log('someone connected');
+      startRedis();
   }); 
 };
 
@@ -12,3 +14,14 @@ exports.callSocket = function () {
  return socket;
 }
 
+exports.getRedis = function () {
+   client.get("coffee", function (err, reply) {
+       return reply.toString(); 
+    });
+};
+
+
+function startRedis(){
+  redis.createClient();
+  redis.set('coffee', '0');
+}
