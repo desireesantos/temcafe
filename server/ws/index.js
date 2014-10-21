3b01b47d3ada97af086bf2309cd6b3ed2904e6d2
Desiree Santos <dsantos@thoughtworks.com>
2014-10-21 20:58:38 -0200

    Remove redis

--- server/ws/index.js
+++ server/ws/index.js
@@ -1,6 +1,4 @@
 var socket;
-var redis = require('redis'),
-    url = require('url');
 
 exports.listen = function (http) {
  socket = require('socket.io').listen(http, require('config').server);
@@ -13,16 +11,3 @@ exports.listen = function (http) {
 exports.callSocket = function () {
  return socket;
 }
-
-exports.createClient = function () {
-    var client;
-    console.log(url);
-    var redisURL = url.parse(process.env.REDISCLOUD_URL);
-    if (process.env.REDISTOGO_URL) {       
-        client = redis.createClient(redisURL.port, redisURL.hostname, options);
-        client.auth(redisURL.auth.split(":")[1]);
-    } else {
-        client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-    }
-    return client;
-};
