3d1dac363bcc438d6e9abfe472ac74096801cfcc
Desiree Santos <dsantos@thoughtworks.com>
2014-10-21 23:25:16 -0200

    Adding Redis

--- server/ws/index.js
+++ server/ws/index.js
@@ -24,7 +24,9 @@ exports.getRedis = function () {
 exports.setRedis = function (newStatus) {
   console.log("***** SET UP *****")
   client.set('coffee', newStatus);
-  console.log(getRedis());
+  console.log(client.get("coffee", function (err, reply) {
+       return reply.toString(); 
+    }););
 }
 
 function startRedis(){
