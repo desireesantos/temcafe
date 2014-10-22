edf6d50c266d28c26ae9a23ab001baf2d6954e47
Desiree Santos <dsantos@thoughtworks.com>
2014-10-21 22:32:46 -0200

    Call websocket

--- server/api/index.js
+++ server/api/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = module.exports = express();
 var Machine =  require('./service/machine');
 var ws = require('../ws');
-var redis = ws.redis();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.put('/:id', function (req, res) {
-  updatedWebClient(redis.getRedis());
+	console.log(ws.getRedis());
+  updatedWebClient(ws.getRedis());
   res.json(new Machine(req.body.status));
 });
 
