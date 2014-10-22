085987ad82bbf29d2476b4497e2631f134305c09
Desiree Santos <dsantos@thoughtworks.com>
2014-10-22 00:23:57 -0200

    Adding Redis

--- server/ws/index.js
+++ server/ws/index.js
@@ -18,7 +18,7 @@ exports.callSocket = function () {
 exports.getRedis = function () {
  console.log(client.get('foo', function (err, reply) {
     reply.toString(); 
-}););
+}));
 };
 
 exports.setRedis = function (newStatus) {
