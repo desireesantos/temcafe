bb97d0444d5af496ed76df3f1b161f51042b7c98
Desiree Santos <dsantos@thoughtworks.com>
2014-11-01 20:46:34 -0200

    Fixing sintaxe error

--- server/ws/index.js
+++ server/ws/index.js
@@ -6,7 +6,7 @@ exports.listen = function (http) {
  socket = require('socket.io').listen(http, require('config').server);
  socket.sockets.on('connection', function (client) {
       client.emit('coffe:level', '0' );
-      console.log('someone connected');
+      console.log('someone connecting ...');
       startRedis();
   }); 
 };
@@ -22,7 +22,7 @@ exports.getRedis = function () {
 };
 
 exports.redis = function (newStatus) {
-  client
+  client;
 }
 
 function startRedis(){
