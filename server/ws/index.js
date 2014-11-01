8c965adc84a8c60bd2d4a8a18be14af6bb64ffa9
Desiree Santos <dsantos@thoughtworks.com>
2014-11-01 21:50:12 -0200

    redis is not setting - fixing

--- server/ws/index.js
+++ server/ws/index.js
@@ -36,5 +36,9 @@ var redisURL = url.parse(process.env.REDISCLOUD_URL);
 client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
 client.set('foo',70);
+client.get('foo', function (err, reply) {
+	console.log("START REDIS");
+    console.log(reply.toString()); 
+});
 }
 
