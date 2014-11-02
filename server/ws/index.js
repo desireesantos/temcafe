7d96170150bd687bebd89ced9a71b330d96b442e
Desiree Santos <dsantos@thoughtworks.com>
2014-11-02 00:15:20 -0200

    changing the start order

--- server/ws/index.js
+++ server/ws/index.js
@@ -1,7 +1,6 @@
 var client;
 var socket;
 
-
 exports.listen = function (http) {
  socket = require('socket.io').listen(http, require('config').server);
  socket.sockets.on('connection', function (client) {
@@ -50,22 +49,3 @@ return client.get("coffee", function (err, reply) {
     reply.toString();
 });
 }
-
-
-client.set("string key", "string val", redis.print);
-client.hset("hash key", "hashtest 1", "some value", redis.print);
-client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-client.hkeys("hash key", function (err, replies) {
-    if (err) {
-        return console.error("error response - " + err);
-    }
-
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-    });
-});
-
-client.quit(function (err, res) {
-    console.log("Exiting from quit command.");
-});
