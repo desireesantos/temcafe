e2f0abe358cb4955c25a922ad2727aa96ab3f3eb
Desiree Santos <dsantos@thoughtworks.com>
2014-11-02 01:24:05 -0200

    Adding mongodb

--- server/ws/index.js
+++ server/ws/index.js
@@ -17,7 +17,7 @@ var mongoUri = process.env.MONGOLAB_URI ||
 mongo.Db.connect(mongoUri, function (err, db) {
   db.collection('temcafe', function(er, collection) {
     collection.insert({'coffee': '10'}, function(er,rs) {
-    	console.log("Record added as "+ db.collection.find(){{ "_id" : ObjectId("526705b4a3559a176784b4af"), "coffee" : "10" }};
+    	console.log("Record added as "+ db.collection.find(){ "_id" : ObjectId("526705b4a3559a176784b4af"), "coffee" : "10" };
     });
   });
 });
