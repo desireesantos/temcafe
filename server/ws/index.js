0d7b2610ab241da23159f9a3c629dcca2eadde06
Desiree Santos <dsantos@thoughtworks.com>
2014-11-02 17:48:44 -0200

    Fixing redis to rec the data

--- server/ws/index.js
+++ server/ws/index.js
@@ -5,7 +5,7 @@ exports.startRedis = function () {
 	var redis = require('redis');	
 	var url = require('url');
 	var redisURL = url.parse(process.env.REDISCLOUD_URL);
-	client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true, return_buffers: true});
+	client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true, detect_buffers: true});
 	client.auth(redisURL.auth.split(":")[1]);
 	client.set('coffee', 10);
 	console.log('Redis ready with value(2) ----> ' + readingCoffeeLevel(client));
