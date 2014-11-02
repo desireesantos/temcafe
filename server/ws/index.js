26cb2a955334159e2cec873323109b4b836cb880
Desiree Santos <dsantos@thoughtworks.com>
2014-11-02 00:07:42 -0200

    changing the start order

--- server/ws/index.js
+++ server/ws/index.js
@@ -1,5 +1,6 @@
 var client;
 var socket;
+var redis;
 
 exports.listen = function (http) {
  socket = require('socket.io').listen(http, require('config').server);
@@ -10,16 +11,18 @@ exports.listen = function (http) {
 };
 
 exports.startRedis = function () {
-var redis = require('redis');	
+redis = require('redis');	
 var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
 
 client.on("connect", function () {
-    console.log("Got redis connection ...")
+    console.log("Got redis connection ...");
 });
-client.set('coffee', 10);
+
+redis.debug_mode = true;
+client.set('coffee', 10, redis.print);
 console.log('Redis ready with value' + testToRead());
 }
 
@@ -41,9 +44,28 @@ client.get('coffee', client.print);
 }
 
 function  testToRead () {
-client.get('coffee', client.print);
-
+return client.get("coffee", function (err, reply) {
+   
+    console.log(reply.length + " replies:");
+    reply.toString();
+});
 }
 
 
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    if (err) {
+        return console.error("error response - " + err);
+    }
 
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+});
+
+client.quit(function (err, res) {
+    console.log("Exiting from quit command.");
+});
