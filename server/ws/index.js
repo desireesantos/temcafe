13a7f30283e296390d706a6c89ac1a34e7334c35
Desiree Santos <dsantos@thoughtworks.com>
2014-11-02 21:21:46 -0200

    Fixing redis to rec the data

--- server/ws/index.js
+++ server/ws/index.js
@@ -1,4 +1,4 @@
-var client;
+var redisClient;
 var socket;
 var getResult = 0;
 
@@ -6,14 +6,17 @@ exports.startRedis = function () {
 	var redis = require('redis');	
 	var url = require('url');
 	var redisURL = url.parse(process.env.REDISCLOUD_URL);
-	client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-	client.auth(redisURL.auth.split(":")[1]);
+	redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+	redisClient.auth(redisURL.auth.split(":")[1]);
+	redisClient.set('coffee', 10);
+	console.log('Redis ready with value(2) ----> ');
 }
 
 exports.listen = function (http) {
 	socket = require('socket.io').listen(http, require('config').server);
 	socket.sockets.on('connection', function (client) {
-	client.emit('coffe:level', readingCoffeeLevel(client, redis_get));
+	client.emit('coffe:level', readingCoffeeLevel(redis, redis_get) );	
+	console.log('someone connecting ...');
 }); 
 }
 
@@ -22,21 +25,21 @@ exports.callSocket = function () {
 }
 
 exports.redis = function () {
- return client;
+ return redisClient;
 }
 
 exports.setRedis = function (value) {
- client.set('coffee', value);
+ redisClient.set('coffee', value);
 }
 
-exports.getRedis = function (client) {
-client.get('coffee', function (err, reply) {
+exports.getRedis = function () {
+redisClient.get('coffee', function (err, reply) {
 	  reply.toString();
 	});
 }
 
-function readingCoffeeLevel (client, callback) {
- client.get('coffee', function(err, value) {
+function readingCoffeeLevel (redisClient, callback) {
+ redisClient.get('coffee', function(err, value) {
         if(err) {
             console.error("error");
         } else {
@@ -46,7 +49,8 @@ function readingCoffeeLevel (client, callback) {
 }
 
 function redis_get (value) {
-  return getResult = value;
+  getResult = value;
+  console.dir("ENTROU " + getResult);
 };
 
 
