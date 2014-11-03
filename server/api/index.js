dc59ab2cfcdd72339668c40ae009547bea7e509f
Desiree Santos <dsantos@thoughtworks.com>
2014-11-02 22:56:58 -0200

    Fixing redis to rec the data

--- server/api/index.js
+++ server/api/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 app.put('/:id', function (req, res) {	
 ws.redis().setRedis(req.body.status);
- updatedWebClient(req.body.status);
+updatedWebClient(req.body.status);
  res.json(new Machine(req.body.status));
 });
 
